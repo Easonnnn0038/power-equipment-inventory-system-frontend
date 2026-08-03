307f6360400e18b9c2a0dcb817280534dc381fd3
Easonnnn0038 <3353186240@qq.com>
2026-08-03 11:54:09 +0800

    清理无用文件，更新项目结构

--- patch-mapper.ps1
+++
@@ -1,4 +0,0 @@
-$src = "c:\Users\33531\IdeaProjects\demo-system-frontend-2\temp\InspectionRecordMapper.java"
-$dst = "c:\Users\33531\IdeaProjects\demo-system -2\src\main\java\com\easonnnnn\demosystem\mapper\InspectionRecordMapper.java"
-Copy-Item -Path $src -Destination $dst -Force
-Write-Host "Done: copied InspectionRecordMapper.java"
